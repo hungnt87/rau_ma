f7b1bc79bf3e47bd87302191f165ebb3fe02d1f2
hungnt <thanhhung.vip@gmail.com>
2023-12-16 01:15:56 +0700

    upload data

--- hero.py
+++ hero.py
@@ -2,11 +2,11 @@
 
 #hero lv1
 WinterWyvern = {
-    WinterWyvern_hero:"lib\image\hero\lv1\WinterWyvern.png",
-    WinterWyvern_lv1:"lib\image\hero\lv1\WinterWyvern_lv1.png"
+    WinterWyvern_hero:"data\image\hero\lv1\WinterWyvern.png",
+    WinterWyvern_lv1:"data\image\hero\lv1\WinterWyvern_lv1.png"
 }
 
 
 #hero lv2
-Luna="lib\image\hero\lv2\Luna.png"
-Luna_lv1="lib\image\hero\lv2\Luna_lv1.png"
+Luna="data\image\hero\lv2\Luna.png"
+Luna_lv1="data\image\hero\lv2\Luna_lv1.png"
